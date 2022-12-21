77ddac08de05b882f6bb10ac50912b0fffa87533
salmamyoussef26 <salmamyoussef1999@gmail.com>
2022-12-21 22:32:28 +0200

    insert into table file

--- create_table.sh
+++ create_table.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+#**********no repeated columns*********
+
+
 echo "Create table"
 
 while true
@@ -22,19 +25,22 @@ do
 done
 #-------------------------
 
+
 declare -i num_of_columns
+#export -p num_of_columns
 read -p "Enter the number of columns" num_of_columns
 
-declare -a columns[$num_of_columns][3]
 
+declare -a columns
 echo "columns information: name, datatype, constraint "
-for (( i=0; i<num_of_columns; i++ ))
+for ((i=0; i<3; i++))
 do
-    for ((j=0; j<3; j++))
+    for ((j=0; j<num_of_columns; j++))
     do
-        read columns[$i][$j]
-        echo -n "${columns[$i][$j]}":"" >>`pwd`/"$table_name metadata"
+        read columns[$i,$j]
+        echo -n ${columns[$i,$j]}":" >>`pwd`/"$table_name metadata"
     done
 
     echo -e "\n" >> `pwd`/"$table_name metadata"
 done
+
--- draft.sh
+++ draft.sh
@@ -1,8 +1,45 @@
-#!/usr/bin/bash
+# #!/bin/bash
 
-read -p "enter an input " input
-if [[ $input == +([a-zA-Z0-9_-]) ]] && [[ $input == [a-zA-Z]* ]];then
-    echo "valid"
-else
-    echo "not valid"
-fi
+# # read -p "enter an input " input
+# # if [[ $input == +([a-zA-Z0-9_-]) ]] && [[ $input == [a-zA-Z]* ]];then
+# #     echo "valid"
+# # else
+# #     echo "not valid"
+# # fi
+# #--------------------------
+# declare -A dictionary
+# declare -a columns_name
+# declare -a data_taypes
+# # animals['key']='value' to set value
+
+# # "${animals[@]}" to expand the values
+
+# # "${!animals[@]}" (notice the !) to expand the keys
+# read -p "enter number of columns" columns_num
+# for ((i=0; i<columns_num; i++))
+# do
+#     read columns_name[$i]
+# done
+
+# for ((i=0; i<columns_num; i++))
+# do
+#     read data_taypes[$i]
+# done
+
+# # key_var='key3'
+# # value_var='value3'
+# # test_dict[$key_var]=$value_var
+
+# for((i=0; i<columns_num; i++))
+# do
+#     dictionary[${columns_name[$i]}]=${data_taypes[$i]} 
+# done
+
+# # for key in "${!test_dict[@]}"; do
+# #     echo "$key ${test_dict[$key]}"
+# # done
+# for key in "${!dictionary[@]}"
+# do
+#     echo $key ":" ${dictionary[$key]}
+# done
+
